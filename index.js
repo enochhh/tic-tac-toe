3b3753f11f5447af0197138a9ca997dc99fc6153
enoch harnanto <enochharnantoo@gmail.com>
2022-03-02 12:19:33 -0800

    Added function to add marks to board

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 let gameGrid = document.getElementById("game-grid");
-
-const Gameboard = (() => {
+let turn = 1;
+const gameBoard = (() => {
     let gameboard = ['','','','','','','','',''];
-    gameGrid.style.setProperty('--grid-rows',3);
-    gameGrid.style.setProperty('grid-cols', 3);
-    for (i = 0; i < 9; i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        gameGrid.appendChild(cell).className = 'board-item'; 
+    const createNew = () => {
+        for (i = 0; i < 9; i++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            gameGrid.appendChild(cell).className = 'board-item';
+            cell.addEventListener('click', displayTurn); 
+        }
+    }
+    return {
+        createNew,
     }
 })(); 
+
+gameBoard.createNew();
+
+function displayTurn(e) {
+        if (turn % 2 == 1 && e.target.innerHTML=='') {
+            e.target.innerHTML = 'x';
+            turn++;
+        }
+        else if(turn % 2 == 0 && e.target.innerHTML=='') {
+            e.target.innerHTML = 'o';
+            turn++;
+        }
+}
+
+const gameController = (() => {
+
+})();
+
+const Player = (choice) => {
+    
+}
